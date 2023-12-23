bd07eee8f2d3174d3ea8fb509f6b5e83d3df7f01
Pau <141435859+pauleenann@users.noreply.github.com>
2023-12-23 17:20:34 +0800

    object pooler in forest map

--- Endlessrunner-ninelives/Assets/PlatformGenerator3.cs
+++ Endlessrunner-ninelives/Assets/PlatformGenerator3.cs
@@ -14,6 +14,8 @@ public class PlatformGenerator3 : MonoBehaviour
     public float distanceBetweenMin;
     public float distanceBetweenMax;
 
+    public ObjectPooler theObjectPool;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +35,13 @@ public class PlatformGenerator3 : MonoBehaviour
             distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
             //new transform.position ng platformgenerator
             transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y,transform.position.z);
-            Instantiate(thePlatform, transform.position, transform.rotation);
+            //Instantiate(thePlatform, transform.position, transform.rotation);
+
+            GameObject newPlatform = theObjectPool.GetPooledObject();
+
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
         }
     }
 }
