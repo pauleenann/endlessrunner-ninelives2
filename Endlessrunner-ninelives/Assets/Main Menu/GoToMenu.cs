0d34eccba9f17b12dcf26defbf6c1a3103996baf
Leanne <116819951+liy4nn4@users.noreply.github.com>
2024-01-15 13:28:07 +0800

    Add files via upload
    Assets for the scene after the first scene of the game

---
+++ Endlessrunner-ninelives/Assets/Main Menu/GoToMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour
+{
+    public void GoToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
