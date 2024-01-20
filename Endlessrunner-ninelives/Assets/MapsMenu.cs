cab99d713dfe83a178dc32aae3948d95ea7d3c68
Pau <141435859+pauleenann@users.noreply.github.com>
2024-01-21 02:27:10 +0800

    update maps

--- Endlessrunner-ninelives/Assets/MapsMenu.cs
+++ Endlessrunner-ninelives/Assets/MapsMenu.cs
@@ -10,16 +10,23 @@ public class MapsMenu : MonoBehaviour
     public TextMeshProUGUI fishText;
     public int totalCoins;
     public int totalFish;
-    public int forestPrice = 100000;
-    public int houseFencePrice = 200000;
-    public int nightMapPrice = 300000;
-    public string defaultLevel;
+    public int forestMapPrice;
+    public int nightMapPrice;
     public string forestLevel;
-    public string houseFencesLevel;
+    public string defaultLevel;
     public string nightLevel;
     public string mainMenuLevel;
     public string mapLevel;
     public GameObject insufficientCoins;
+    public GameObject mapUnlocked;
+    public GameObject nightCoin;
+    public GameObject nightPrice;
+    public GameObject nightPlay;
+    public GameObject buyNight;
+    public GameObject forestCoin;
+    public GameObject forestPrice;
+    public GameObject forestPlay;
+    public GameObject buyForest;
     
     void Start()
     {
@@ -54,23 +61,72 @@ public class MapsMenu : MonoBehaviour
 
     public void Forest()
     {
-        
-        
-        Application.LoadLevel(forestLevel);
-    }
-
-    public void HouseFences()
-    {
-        Application.LoadLevel(houseFencesLevel);
+        if(forestMapPrice != 0)
+        {
+            buyForest.SetActive(true);  
+        }
+        else
+        {
+            Application.LoadLevel(forestLevel);
+        }
+         
+        //if (totalCoins >= nightMapPrice)
+        //{
+        //    Application.LoadLevel(forestLevel);
+        //}
+        //else
+        //{
+        //    insufficientCoins.SetActive(true);
+        //}
     }
 
     public void Night()
     {
-        if(totalCoins >= nightMapPrice)
+        Debug.Log(totalCoins);
+        Debug.Log(nightMapPrice);
+        if(nightMapPrice != 0){
+            buyNight.SetActive(true);
+        }
+        else if (nightMapPrice == 0)
         {
             Application.LoadLevel(nightLevel);
         }
-        
-        
+        else if(totalCoins >= nightMapPrice)
+        {
+            mapUnlocked.SetActive(true);
+            nightCoin.SetActive(false);
+            nightPrice.SetActive(false);
+            nightPlay.SetActive(true);
+            totalCoins -= nightMapPrice;
+            nightMapPrice = 0;
+            coinsText.text = "" + totalCoins;
+            //LoadLevel(nightLevel);
+        }
+        else
+        {
+         
+            insufficientCoins.SetActive(true);
+        }
     }
+
+    public void BuyMap()
+    {
+        if (totalCoins >= forestMapPrice)
+        {
+            mapUnlocked.SetActive(true);
+            forestCoin.SetActive(false);
+            forestPrice.SetActive(false);
+            forestPlay.SetActive(true);
+            totalCoins -= forestMapPrice;
+            forestMapPrice = 0;
+            coinsText.text = "" + totalCoins;
+            //LoadLevel(nightLevel);
+        }
+        else
+        {
+
+            insufficientCoins.SetActive(true);
+        }
+    }
+    
 }
